beb69a7878b2083b0849830c0cdf0b305f5f54eb
matheusburey <matheushburei.99@gmail.com>
2023-09-11 21:09:40 -0300

    config views base

--- core/views.py
+++ core/views.py
@@ -1,6 +1,45 @@
-from django.shortcuts import HttpResponse
+from rest_framework.response import Response
+from rest_framework.serializers import ModelSerializer
+from rest_framework.views import APIView
+
+from core.models import Category
 
 
-# Create your views here.
-def teste(request):
-    return HttpResponse({"message": "Hello, world. You're at the livraria index."})
+class CategorySerializer(ModelSerializer):
+    """Serializer class that serializes data from the Category model."""
+
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class CategoryView(APIView):
+    """View class that retrieves data from the Category model and returns it as a JSON response."""
+
+    def get(self, _):
+        """Retrieves data from the Category model and returns it as a JSON response.
+
+        Parameters:
+        - request: The HTTP request object.
+
+        Returns:
+        - HttpResponse: The formatted data as a JSON response.
+        """
+        data = Category.objects.all()
+        serializer = CategorySerializer(data, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        """Create a new category.
+
+        Args:
+            request (HttpRequest): The HTTP request object.
+
+        Returns:
+            Response: The HTTP response object containing the newly created category data.
+        """
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
--- livraria/urls.py
+++ livraria/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
-from core.views import teste
+from core import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", teste),
+    path("api/category/", views.CategoryView.as_view()),
 ]
